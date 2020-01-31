d8abd83d6375c4f3ea858916db89ccb7619375cc
Jagepard <dankorot@gmail.com>
2020-01-31 17:55:10 +0300

    update

---
+++ src/Behavioral/ChainOfResponsibility/Main.java
@@ -0,0 +1,26 @@
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
+package Behavioral.ChainOfResponsibility;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ChainInterface chain = new Chain();
+        HandlerInterface notice = new NoticeHandler();
+        HandlerInterface warning = new WarningHandler();
+        HandlerInterface error = new ErrorHandler();
+
+        try {
+            chain.addToChain(notice);
+            chain.addToChain(warning);
+            chain.addToChain(error);
+            chain.execute(notice.getClass().getName());
+            chain.execute(warning.getClass().getName());
+            chain.execute(error.getClass().getName());
+        } catch (Exception e) {
+            System.out.printf("Caught exception: %s \n", e.getLocalizedMessage());
+        }
+    }
+}
